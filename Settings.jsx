f39840af3d06ef9eab43de4483269dc377c3b95a
Pradeep Baddapuri <98583284+pradeepb007@users.noreply.github.com>
2024-07-15 17:05:38 +0530

    Update Settings.jsx

--- Settings.jsx
+++ Settings.jsx
@@ -56,3 +56,66 @@ const Settings = () => {
 };
 
 export default Settings;
+
+
+import React, { useState, useEffect } from "react";
+import { Box, Checkbox, FormControlLabel, Button, Accordion, AccordionSummary, AccordionDetails, Typography } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { useDispatch, useSelector } from "react-redux";
+import { getSettings, updateSettings, toggleSetting } from "./settingsSlice";
+
+const stepFields = {
+  0: ["id", "firstname", "lastname"],
+  1: ["email", "phone"],
+  2: ["address"],
+};
+
+const Settings = () => {
+  const dispatch = useDispatch();
+  const { settings } = useSelector((state) => state.settings);
+
+  useEffect(() => {
+    dispatch(getSettings());
+  }, [dispatch]);
+
+  const handleCheckboxChange = (field) => {
+    dispatch(toggleSetting({ field }));
+  };
+
+  const handleSave = () => {
+    dispatch(updateSettings(settings));
+  };
+
+  return (
+    <Box>
+      <h1>Settings</h1>
+      {Object.keys(stepFields).map((step) => (
+        <Accordion key={step}>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography>Step {parseInt(step) + 1}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            {stepFields[step].map((field) => (
+              <FormControlLabel
+                key={field}
+                control={
+                  <Checkbox
+                    checked={settings[field] || false}
+                    onChange={() => handleCheckboxChange(field)}
+                  />
+                }
+                label={field}
+              />
+            ))}
+          </AccordionDetails>
+        </Accordion>
+      ))}
+      <Button variant="contained" color="primary" onClick={handleSave}>
+        Save
+      </Button>
+    </Box>
+  );
+};
+
+export default Settings;
+
